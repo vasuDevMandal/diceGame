41461df3784b539d03a32ec40b367feb16cf54a9
Vasu Dev Mandal <code.vasu@gmail.com>
2021-10-17 09:20:18 +0530

    added mySplit method

--- scripts/polyfill.js
+++ scripts/polyfill.js
@@ -13,3 +13,30 @@ Array.prototype.myReverse=function(){
 let array=[1,2,3,4,5,6,7,8];
 array.myReverse();
 console.log(array);
+
+
+// mySplit
+
+String.prototype.mySplit = (sep = "") => {
+  const res = [];
+  let temp = "";
+  for (let i = 0; i < str.length; i++) {
+    const el = str[i];
+    if (el === sep || (sep === "" && temp)) {
+      res.push(temp);
+      temp = "";
+    }
+    if (el !== sep) {
+      temp += el;
+    }
+  }
+  if (temp) {
+    res.push(temp);
+    temp = "";
+  }
+  return res;
+};
+
+
+const str = "this is osme string";
+console.log(str.mySplit(" "));
